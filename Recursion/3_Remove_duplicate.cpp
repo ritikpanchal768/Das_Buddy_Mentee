71d7589e8f11bce94f3d67fea3bb24a6db393a9f
Ritik Panchal <44132205+ritikpanchal768@users.noreply.github.com>
2021-05-26 18:06:44 +0530

    Add files via upload

---
+++ Recursion/3_Remove_duplicate.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+void removeDuplicates(char s[])
+{
+    if (s[0] == '\0')
+        return;
+    if (s[0] == s[1]) 
+    {
+        int i = 0;
+        while (s[i] != '\0') {
+            s[i] = s[i + 1];
+            i++;
+        }
+        removeDuplicates(s);
+    }
+ 
+    removeDuplicates(s + 1);
+}
+ 
+int main()
+{
+    char s[10000];
+    cin>>s;
+    removeDuplicates(s);
+    cout << s << endl;
+
+ 
+    return 0;
+}
